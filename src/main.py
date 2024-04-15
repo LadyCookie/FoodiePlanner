a278f782f4773bf66342944b3fb9d52ec103e94c
LadyCookie <32516903+LadyCookie@users.noreply.github.com>
2024-04-15 21:44:26 +0200

    [Feature] Added color to tabs

--- src/main.py
+++ src/main.py
@@ -26,12 +26,55 @@ root.title("Foodie Planner")
 # TABVIEW #
 #---------#
 
+# CONSTS #
+
+tab_1_name = 'Planificateur'
+tab_1_color = 'lightskyblue'
+tab_1_hover_color = 'lightskyblue1'
+tab_1_bg_color = 'azure1'
+
+tab_2_name = 'Livre de recette'
+tab_2_color = 'mediumpurple3'
+tab_2_hover_color = 'mediumpurple2'
+tab_2_bg_color = 'lavender'
+
+tab_3_name = 'Carnet de suivi'
+tab_3_color = 'seagreen3'
+tab_3_hover_color = 'seagreen2'
+tab_3_bg_color = 'honeydew1'
+
+#--------#
+
 tabview = customtkinter.CTkTabview(master = root)
 tabview.pack(side='bottom', fill='both', expand=True, padx = 20, pady = 20)
 
-tab_1 = tabview.add('Planificateur')
-tab_2 = tabview.add('Livre de recette')
-tab_3 = tabview.add('Carnet de suivi')
+tab_1 = tabview.add(tab_1_name)
+tab_2 = tabview.add(tab_2_name)
+tab_3 = tabview.add(tab_3_name)
+
+def change_tabview_color_scheme(tabview):
+    active_tab = tabview.get()
+    if(active_tab == tab_1_name):
+        tabview.configure(segmented_button_selected_color = tab_1_color)
+        tabview.configure(segmented_button_selected_hover_color = tab_1_hover_color)
+        tabview.configure(segmented_button_unselected_hover_color = tab_1_hover_color)
+        tabview.configure(fg_color = tab_1_bg_color)
+    if(active_tab == tab_2_name):
+        tabview.configure(segmented_button_selected_color = tab_2_color)
+        tabview.configure(segmented_button_selected_hover_color = tab_2_hover_color)
+        tabview.configure(segmented_button_unselected_hover_color = tab_2_hover_color)
+        tabview.configure(fg_color = tab_2_bg_color)
+    if(active_tab == tab_3_name):
+        tabview.configure(segmented_button_selected_color = tab_3_color)
+        tabview.configure(segmented_button_selected_hover_color = tab_3_hover_color)
+        tabview.configure(segmented_button_unselected_hover_color = tab_3_hover_color)
+        tabview.configure(fg_color = tab_3_bg_color)
+    
+tabview.configure(segmented_button_selected_color = tab_1_color)
+tabview.configure(segmented_button_selected_hover_color = tab_1_hover_color)
+tabview.configure(segmented_button_unselected_hover_color = tab_1_hover_color)
+tabview.configure(command = lambda: change_tabview_color_scheme(tabview))
+tabview.configure(fg_color = tab_1_bg_color)
 
 #------#
 # MAIN #
